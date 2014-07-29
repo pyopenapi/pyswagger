226a24286e7b017c0f96637208bb5ef5cd931a1b
mission.liao <missionaryliao@gmail.com>
2014-07-29 13:58:53 +0800

    reference objects, not used in parsing stage.
    explict declaration of objects instead of using json-dict.

---
+++ pyswagger/obj.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import
+
+
+class Base(object):
+    """
+    """
+
+class Scope(Base):
+    """
+    """
+
+class LoginEndpoint(Base):
+    """
+    """
+
+class Implicit(Base):
+    """
+    """
+
+class TokenRequestEndpoint(Base):
+    """
+    """
+
+class Token(Base):
+    """
+    """
+
+class AuthorizationCode(Base):
+    """
+    """
+
+class GrantType(Base):
+    """
+    """
+
+class Authorization(Base):
+    """
+    """
+
+class ResponseMessage(Base):
+    """
+    """
+
+class Parameter(Base):
+    """
+    """
+
+class Operation(Base):
+    """
+    """
+
+class Api(Base):
+    """
+    """
+
+class Model(Base):
+    """
+    """
+
+class Resource(Base):
+    """
+    """
+
+class Info(Base):
+    """
+    """
+
