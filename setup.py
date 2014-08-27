ce6096fa27897a083738ac27f14742d15b9396d8
mission.liao <missionaryliao@gmail.com>
2014-08-27 17:59:58 +0800

    prepare for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'pyswagger',
+    packages = ['pyswagger'], # this must be the same as the name above
+    version = '0.0.1',
+    description = 'A type safe Swagger Client',
+    author = 'Mission Liao',
+    author_email = 'missionaryliao@gmail.com',
+    url = 'https://github.com/AntXlab/pyswagger', # use the URL to the github repo
+    download_url = 'https://github.com/AntXlab/pyswagger/tarball/0.0.1', # I'll explain this in a second
+    keywords = ['swagger', 'REST'], # arbitrary keywords
+    classifiers = [],
+)
