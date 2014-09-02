78c98335419e0cd817f0ab48c24503a05f194c80
mission.liao <missionaryliao@gmail.com>
2014-09-02 19:58:11 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'pyswagger',
     packages = find_packages(exclude=['*.tests.*']),
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'A type safe Swagger Client',
     author = 'Mission Liao',
     author_email = 'missionaryliao@gmail.com',
