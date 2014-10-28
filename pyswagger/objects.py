e0c1ae7c8ca2dbf6fde8ea714aeb513c18522866
mission.liao <missionaryliao@gmail.com>
2014-10-28 15:16:34 +0800

    add parser & objects for swagger 2.0

---
+++ pyswagger/objects.py
@@ -0,0 +1,226 @@
+from __future__ import absolute_import
+from ...base import BaseObj, FieldMeta
+import six
+
+
+class Item(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Item Object
+    """
+
+    __swagger_fields__ = [
+        'type',
+        'format',
+        'items',
+        'collectionFormat',
+        'default',
+        'maximum',
+        'exclusiveMaximum',
+        'minimum',
+        'exclusiveMinimum',
+        'maxLength',
+        'minLength',
+        'pattern',
+        'maxItems',
+        'minItems',
+        'uniqueItems',
+        'enum'
+    ]
+
+
+class Schema(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Schema Object
+    """
+
+    __swagger_fields__ = [
+        '$ref',
+        'format',
+        'default',
+        'multipleOf',
+        'maximum',
+        'exclusiveMaximum',
+        'minimum',
+        'exclusiveMinimum',
+        'maxLength',
+        'minLength',
+        'pattern',
+        'maxItems',
+        'minItems',
+        'uniqueItems',
+        'maxProperties',
+        'minProperties',
+        'required',
+        'enum',
+        'type',
+
+        'items',
+        'allOf',
+        'properties',
+
+        'discriminator',
+        'readOnly',
+    ]
+    __swagger_rename__ = {'$ref', 'ref'}
+
+
+class Swagger(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Swagger Object
+    """
+
+    __swagger_fields__ = [
+        'swagger',
+        'info',
+        'host',
+        'basePath',
+        'schemes',
+        'consumes',
+        'produces',
+        'paths',
+        'definitions',
+        'parameters',
+        'responses',
+        'securityDefinitions',
+        'security',
+        'tags',
+    ]
+
+
+class Info(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Info Object
+    """
+
+    __swagger_fields__ = ['version']
+
+
+class Parameter(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Parameter Object
+    """
+
+    __swagger_fields__ = [
+        # Reference Object
+        '$ref',
+
+        'name',
+        'in',
+        'required',
+
+        # body parameter
+        'schema',
+
+        # other parameter
+        'type',
+        'format',
+        'items',
+        'collectionFormat',
+        'default',
+        'maximum',
+        'exclusiveMaximum',
+        'minimum',
+        'exclusiveMinimum',
+        'maxLength',
+        'minLength',
+        'pattern',
+        'maxItems',
+        'minItems',
+        'uniqueItems',
+        'enum',
+        'multipleOf',
+    ]
+    __swagger_rename__ = {'$ref', 'ref'}
+
+
+class Header(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Header Object
+    """
+
+    __swagger_fields__ = [
+        'type',
+        'format',
+        'items',
+        'collectionFormat',
+        'default',
+        'maximum',
+        'exclusiveMaximum',
+        'minimum',
+        'exclusiveMinimum',
+        'maxLength',
+        'minLength',
+        'pattern',
+        'maxItems',
+        'minItems',
+        'uniqueItems',
+        'enum',
+        'multipleOf',
+    ]
+
+
+class Response(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Response Object
+    """
+
+    __swagger_fields__ = [
+        # Reference Object
+        '$ref',
+
+        'schema',
+        'headers',
+    ]
+    __swagger_rename__ = {'$ref': 'ref'}
+
+
+class Operation(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Operation Object
+    """
+
+    __swagger_fields__ = [
+        'tags',
+        'operationId',
+        'consumes',
+        'produces',
+        'parameters',
+        'responses',
+        'deprecated',
+        'security'
+    ]
+
+
+class PathItem(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Path Item Object
+    """
+
+    __swagger_fields__ = [
+        # Reference Object
+        '$ref',
+
+        'get',
+        'put',
+        'post',
+        'delete',
+        'options',
+        'head',
+        'patch',
+        'parameters',
+    ]
+    __swagger_rename__ = {'$ref': 'ref'}
+
+
+class SecurityScheme(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Security Scheme Object
+    """
+
+    __swagger_fields__ = [
+        'type',
+        'name',
+        'in',
+        'flow',
+        'authorizationUrl',
+        'tokenUrl',
+        'scopes',
+    ]
+
+
+class Tag(six.with_metaclass(FieldMeta, BaseObj)):
+    """ Tag Object
+    """
+
+    __swagger_fields__ = ['name']
+
