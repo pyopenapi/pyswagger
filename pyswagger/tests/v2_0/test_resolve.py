3584017b82e9851f6f4f379b5be14fb6a973f7f1
mission.liao <missionaryliao@gmail.com>
2014-12-02 13:58:09 +0800

    add test case

--- pyswagger/tests/v2_0/test_resolve.py
+++ pyswagger/tests/v2_0/test_resolve.py
@@ -23,3 +23,33 @@ class ResolvePathItemTestCase(unittest.TestCase):
         self.assertTrue(a.get.operationId, 'a.get')
         self.assertTrue(a.put.operationId, 'c.put')
         self.assertTrue(a.post.operationId, 'd.post')
+
+
+class ResolveTestCase(unittest.TestCase):
+    """ test for $ref other than PathItem """
+
+    @classmethod
+    def setUpClass(kls):
+        kls.app = SwaggerApp._create_(get_test_data_folder(                            
+            version='2.0',                                                                 
+            which=os.path.join('resolve', 'other')                                     
+        ))                                                                             
+
+    def test_schema(self):
+        """ make sure $ref to Schema works """
+        p = self.app.s('/a').get
+
+        self.assertEqual(id(p.parameters[1].schema.ref_obj), id(self.app.resolve('#/definitions/d1')))
+
+    def test_parameter(self):
+        """ make sure $ref to Parameter works """
+        p = self.app.s('/a').get
+
+        self.assertEqual(id(p.parameters[0].ref_obj), id(self.app.resolve('#/parameters/p1')))
+
+    def test_response(self):
+        """ make sure $ref to Response works """
+        p = self.app.s('/a').get
+
+        self.assertEqual(id(p.responses['default'].ref_obj), id(self.app.resolve('#/responses/r1')))
+
