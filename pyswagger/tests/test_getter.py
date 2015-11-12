3c46595194086a0febc2f8d92c3c472602e4d826
mission-liao <missionaryliao@gmail.com>
2015-11-12 08:57:42 +0800

    [test] add case
    https://github.com/mission-liao/pyswagger/pull/43

--- pyswagger/tests/test_getter.py
+++ pyswagger/tests/test_getter.py
@@ -1,4 +1,5 @@
 from pyswagger import SwaggerApp
+from pyswagger.getter import UrlGetter
 from .utils import get_test_data_folder
 import unittest
 import os
@@ -26,5 +27,19 @@ class GetterTestCase(unittest.TestCase):
             which='random_file_name'
         )
         path = os.path.join(path, 'test_random.json')
-         # should not raise ValueError
+        # should not raise ValueError
         app = SwaggerApp.create(path)
+
+    def test_local_path_with_custome_getter(self):
+        """ make sure path would be assigned when
+        passing a getter class
+        """
+        cls = UrlGetter
+        path = get_test_data_folder(
+            version='2.0',
+            which='random_file_name'
+        )
+        path = os.path.join(path, 'test_random.json')
+
+        # should not raise errors
+        app = SwaggerApp.load(path, getter=cls)
