7970f0421073871677fb01c7ec01178d14fedaed
mission.liao <missionaryliao@gmail.com>
2017-08-15 22:29:57 +0800

    Add pyswagger.__version__

--- pyswagger/__init__.py
+++ pyswagger/__init__.py
@@ -1,8 +1,10 @@
+__version__ = '0.8.34'
+
 from .getter import Getter
 from .core import App, Security
-
 
 # backward compatible
 SwaggerApp = App
 SwaggerSecurity = Security
 SwaggerAuth = SwaggerSecurity
+
